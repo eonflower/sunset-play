13308587eebcfc87dd332c2db0e6b61a33261404
Aloe Nelson <aloe.eon@gmail.com>
2023-07-10 16:23:24 -0700

    added parallax scroll and placeholder component

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,29 @@ import picture from "./assets/picture.svg"
 import sun from "./assets/sun.svg"
 import treesBack from "./assets/trees-back.svg"
 import trees from "./assets/trees.svg"
+import Main from './components/Main'
 
 function App() {
 
   return (
     <div className='app'>
-    <img className="background" src={picture} alt='' style={{width: '100%'}} />
-    <img className="trees" src={trees} alt='' style={{width: '100%'}} />
-    <img className="trees-back" src={treesBack} alt='' style={{width: '100%'}} />
-    <img className="sun" src={sun} alt='' style={{width: '25%'}}/>
-    <img className="cloud-left" src={cloudLeft} alt='' style={{width: '20%'}} />
-    <img className="cloud-right" src={cloudRight} alt='' style={{width: '25%'}} />
-    <img className="cloud-right-btm" src={cloudRightBtm} alt='' style={{width: '25%'}} />
-    <img className="cloud-middle" src={cloudMiddle} alt='' style={{width: '20%'}} />
+      <div className='container'>
+      <div className='sun' />
+      {/* <img className="background" src={picture} alt='' style={{width: '100%'}} /> */}
+      <div className='trees-container'>
+        <img className="trees" src={trees} alt='' style={{width: '100%'}} />
+        <img className="trees-back" src={treesBack} alt='' style={{width: '100%'}} />
+        <div className='scroll-background'>
+        <Main />
+        </div>
+      </div>
+      <img className="sun" src={sun} alt='' style={{width: '20%'}}/>
+      <img className="cloud-left" src={cloudLeft} alt='' style={{width: '20%'}} />
+      <img className="cloud-right" src={cloudRight} alt='' style={{width: '25%'}} />
+      <img className="cloud-right-btm" src={cloudRightBtm} alt='' style={{width: '25%'}} />
+      <img className="cloud-middle" src={cloudMiddle} alt='' style={{width: '20%'}} />
+      </div>
+      
     
     </div>
   )
---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Main() {
+	return (
+		<div className='main-container'>
+			<h2>Welcome to the Sunset</h2>
+		</div>
+	)
+}
