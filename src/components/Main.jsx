16ac24a453d85047310624981245a3bd34708cc3
Aloe Nelson <aloe.eon@gmail.com>
2023-07-12 09:49:54 -0700

    added poem temporarily

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,26 @@ import React from 'react'
 export default function Main() {
 	return (
 		<div className='main-container'>
-			<h2>Welcome to the Sunset</h2>
+			<h2>The Truth of Pine</h2>
+			<h4>a short poem by Aloe</h4>
+			<p>
+				What if pining meant that I could finally be a tree? Instead of pining over love, I'd be a hemlock. 
+				Don't mistake these tiny cones for a lack of power or jurisdiction in the world. 
+				For not everything big and mighty is the best. 
+			</p><p>
+				Fragile squishy flesh to a thickly grooved shell. 
+				From wispy beach curls to needle filled twigs. From long lost dreams of my childhood to present 
+				day reality. Listen to the laughter of the wind while huddling in closely. There is shelter from 
+				the tears of the clouds, but don't forget to remind them that you love them. You see, the sun will 
+				hear the sweet whispers through the thicket of the fog, but the vapor of the clouds will dissipate 
+				if you let it. 
+			</p><p>
+				Tomorrow I'll hang my branches to the side, but not in sorrow you see. For now that 
+				I am a pine, I am relinquished of what it means to struggle as a human. I am sedentary and I like it. 
+				The bugs will find safety beneath this bark-ish skin. The birds will find home in my branches. 
+			</p><p>
+				Pining. For I am living my truth in the world. 
+			</p>
 		</div>
 	)
 }
